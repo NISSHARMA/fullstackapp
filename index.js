9fcef7067ad9922348a30147926255c3d0d5ac70
unknown <Nisha Sharma>
2023-03-26 13:31:54 +0530

    Fullstack app

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors=require("cors")
 const { auth } = require("./middleware/auth.middleware")
 const app = express()
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
 app.use("/user", registerRoute)
 app.use(auth)
